a4f79161e8af968ffcecc6ef06c59ed6737e13d1
laurenzalt <laurenz.altendorfer@edu.fh-joanneum.at>
2024-04-09 10:22:32 +0200

    Initial Commit

---
+++ QuickNote/QuickNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickNoteApp.swift
+//  QuickNote
+//
+//  Created by Altendorfer Laurenz on 09.04.24.
+//
+
+import SwiftUI
+
+@main
+struct QuickNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
